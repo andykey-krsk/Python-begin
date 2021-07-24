ef15b5ad3e741e3920da053e1570d79a18c713f4
andykey-krsk <andykey@bk.ru>
2021-07-25 03:10:35 +0700

    Домашнее задание №3. fix

--- lesson_3/task_3.py
+++ lesson_3/task_3.py
@@ -2,17 +2,20 @@
 # и возвращает сумму наибольших двух аргументов.
 
 def my_func(num_1, num_2, num_3):
-    if num_1 > num_2:
-        max_1 = num_1
+    max_1 = num_1
+    if max_1 > num_2:
+        max_2 = num_2
     else:
         max_1 = num_2
+        max_2 = num_1
 
-
+    if max_2 < num_3:
+        max_2 = num_3
 
     return max_1 + max_2
 
 
 args = input('введите три числа, разделенные пробелами: ').split(' ')
 
-print(my_func(float(args[0]), float(args[1]), float(args[3])))
+print(my_func(float(args[0]), float(args[1]), float(args[2])))
 
